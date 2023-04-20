cb77e65b3d55eb5a22930fab92b51c9a28fe2932
Daniel Guzdek <daniel.guzdek@edge1s.com>
2023-04-20 09:32:20 +0200

    Create useFetch custom hook to provide fetching functionality.

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { Difficulty } from "../ts/enums/app_enums";
+import { QuestionsState } from "../ts/types/app_types";
+
+export const useFetch = ({ url }: any) => {
+  const [response, setResponse] = useState({});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchQuestions = async (
+    amount: number,
+    category: string,
+    difficulty: Difficulty
+  ): Promise<QuestionsState[]> => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=${category}difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(endpoint)).json();
+    setResponse(data);
+    console.log(data);
+    return data;
+  };
+
+  useEffect(() => {
+    fetchQuestions(10, "Sport", Difficulty.EASY);
+  }, [url]);
+
+  return { response, error, loading };
+};
