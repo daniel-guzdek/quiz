258b730b0d7d61165b9d82a32c1af218c625a7db
Daniel Guzdek <daniel.guzdek@edge1s.com>
2023-04-20 09:23:18 +0200

    Add basic structure & logic to set quiz mode and players' settings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
-const App = () => {
-  return <div className="App">Quiz</div>;
+import { useSelector, useDispatch } from "react-redux";
+import QuizModesPanel from "./components/QuizModesPanel";
+import { RootState } from "./state/reducers";
+import PlayersMode from "./views/PlayersMode";
+import UserForm from "./views/UserForm";
+
+const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const { players_mode_is_set, is_form_valid } = useSelector(
+    (state: RootState) => state.quiz
+  );
+
+  const wholeState = useSelector((state: RootState) => state.quiz);
+  console.log(wholeState);
+
+  const handleReset = () => {
+    dispatch({
+      type: "reset-store",
+    });
+  };
+
+  return (
+    <div>
+      <pre>{JSON.stringify(wholeState)}</pre>
+      <h1>Quiz</h1>
+      <button onClick={() => handleReset()}>Reset</button>
+      <div style={{ display: `${is_form_valid ? "none" : "block"}` }}>
+        {!players_mode_is_set ? <PlayersMode /> : <UserForm />}
+      </div>
+      {is_form_valid && <QuizModesPanel />}
+    </div>
+  );
 };
 
 export default App;
