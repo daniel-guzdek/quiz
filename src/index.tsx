4239790fbcbb2661a1888571156bfd21a3421199
Daniel Guzdek <daniel.guzdek@edge1s.com>
2023-04-20 09:26:03 +0200

    Provide updates according to React 18 version.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import * as ReactDOMClient from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./state";
 import App from "./App";
 
 const root = ReactDOMClient.createRoot(
   document.getElementById("root") as HTMLDivElement
 );
-root.render(<App />);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
